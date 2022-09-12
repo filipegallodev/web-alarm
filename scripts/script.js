a7e0b341791ea3e14d28606c246ab89fa3cb45f6
Filipe G <fgallo_dev@hotmail.com>
2022-09-12 14:34:14 -0300

    Adicionando o som do alarme

---
+++ scripts/script.js
@@ -0,0 +1,56 @@
+function timeGet() {
+  let localTimeHours = new Date().getHours();
+  let localTimeMinutes = minutesGet();
+  let localTimeSeconds = secondsGet();
+
+  let localTime = localTimeHours + ':'
+    + localTimeMinutes + ':'
+    + localTimeSeconds;
+
+  let timeDoc = document.querySelector('#localtime');
+
+  timeDoc.innerHTML = localTime;
+
+  alarmSet(localTime);
+}
+
+function minutesGet() {
+  let localTimeMinutes = new Date().getMinutes();
+  if (localTimeMinutes < 10) {
+    localTimeMinutes = "0" + localTimeMinutes;
+  }
+  return localTimeMinutes;
+}
+
+function secondsGet() {
+  let localTimeSeconds = new Date().getSeconds();
+  if (localTimeSeconds < 10) {
+    localTimeSeconds = "0" + localTimeSeconds;
+  }
+  return localTimeSeconds;
+}
+
+function alarmSet(localTime) {
+  localTime = localTime.slice(0, 5);
+  console.log(`Horário: ${localTime}`);
+
+  button.onclick = function () {
+    let alarmTime = document.querySelector('#alarm-choice').value;
+    document.querySelector('#alarmtime').innerHTML = alarmTime;
+    console.log(`Horário do alarme: ${alarmTime}`);
+  }
+
+  let alarmTime = document.querySelector('#alarmtime');
+
+  alarmTime = alarmTime.textContent.trim();
+
+  if (localTime == alarmTime) {
+    sound.play();
+  }
+}
+
+let button = document.querySelector('#alarm-button-save');
+
+const sound = new Audio("src/audio/alarm.wav");
+
+setInterval(timeGet, 1000);
